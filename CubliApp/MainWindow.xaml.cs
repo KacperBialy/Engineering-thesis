067a1907252c1460c56bb263a6364ddceb66e4da
Kacper <kacper.bialy@student.put.poznan.pl>
2020-11-30 22:53:54 +0100

    Initial configuration of UART transmission

--- CubliApp/MainWindow.xaml.cs
+++ CubliApp/MainWindow.xaml.cs
@@ -1,19 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO.Ports;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.IO.Ports;
-using System.Threading;
 
 namespace CubliApp
 {
@@ -24,11 +13,12 @@ namespace CubliApp
     {
         public int boudRate { get; set; }
         public int dataBits { get; set; }
-        public int stopBits { get; set; }
+        public string parity { get; set; }
+        public string stopBits { get; set; }
         public MainWindow()
-            {
-                InitializeComponent();
-            }
+        {
+            InitializeComponent();
+        }
 
         private void btn_ReScan_Click(object sender, RoutedEventArgs e)
         {
@@ -44,12 +34,29 @@ namespace CubliApp
         private void RadioButton_Checked(object sender, RoutedEventArgs e)
         {
             RadioButton radioButton = (RadioButton)sender;
-            if(radioButton.GroupName == "BaudRate")
+            if (radioButton.GroupName == "BaudRate")
                 boudRate = int.Parse(radioButton.Content.ToString());
-            else if(radioButton.GroupName == "DataBits")
+            else if (radioButton.GroupName == "DataBits")
                 dataBits = int.Parse(radioButton.Content.ToString());
-            else if(radioButton.GroupName == "StopBits")
-                stopBits = int.Parse(radioButton.Content.ToString());
+            else if (radioButton.GroupName == "Parity")
+                parity = radioButton.Content.ToString();
+            else if (radioButton.GroupName == "StopBits")
+                stopBits = radioButton.Content.ToString();
+
+        }
+
+        private void btn_Connect_Click(object sender, RoutedEventArgs e)
+        {
+            SerialPort _serialPort;
+
+            // Create a new SerialPort object with default settings.
+            _serialPort = new SerialPort();
+            // Allow the user to set the appropriate properties.
+            _serialPort.PortName = "COM4";
+            _serialPort.BaudRate = boudRate;
+            _serialPort.Parity = (Parity)Enum.Parse(typeof(Parity), parity, true);
+            _serialPort.DataBits = dataBits;
+            _serialPort.StopBits = (StopBits)Enum.Parse(typeof(StopBits), stopBits, true);
 
         }
     }
