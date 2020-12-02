a7a2d95b5694897ab274189ba85ec7571b9a1a25
Kacper <kacper.bialy@student.put.poznan.pl>
2020-12-02 22:49:08 +0100

    Establish communication between computer and stm32 Nucleo
    Added reading messages in a separate thread and displaying them on the window and sending string to stm32.Bugs with buttons have been fixed.

--- CubliApp/Bluetooth.cs
+++ CubliApp/Bluetooth.cs
@@ -1,11 +1,150 @@
 ﻿using System;
 using System.IO.Ports;
+using System.Text;
 using System.Threading;
+using System.Windows;
 
 namespace CubliApp
 {
     class Bluetooth
     {
-       
+        private SerialPort _serialPort;
+
+        public int BoudRate { get; set; }
+        public int DataBits { get; set; }
+        public string Parity { get; set; }
+        public string StopBits { get; set; }
+        public string PortName { get; set; }
+        public StringBuilder dataReceived { get; set; }
+        public StringBuilder dataSend { get; set; }
+        private Thread readThread { get; set; }
+        private bool IsPortOpen { get; set; }
+        public Bluetooth()
+        {
+            dataReceived = new StringBuilder();
+            dataSend = new StringBuilder();
+        }
+        private bool Configuration()
+        {
+            bool isConfigurationOK = false;
+            if (PortName != null)
+            {
+                _serialPort = new SerialPort();
+
+                _serialPort.PortName = PortName;
+                _serialPort.BaudRate = BoudRate;
+                _serialPort.Parity = (Parity)Enum.Parse(typeof(Parity), Parity, true);
+                _serialPort.DataBits = DataBits;
+                _serialPort.StopBits = (StopBits)Enum.Parse(typeof(StopBits), StopBits, true);
+                _serialPort.ReadTimeout = 5000;
+                _serialPort.WriteTimeout = 5000;
+
+                isConfigurationOK = true;
+
+                return isConfigurationOK;
+            }
+            else
+            {
+                MessageBox.Show("Select a port","Port problem",MessageBoxButton.OK,MessageBoxImage.Warning);
+                return isConfigurationOK;
+            }
+        }
+        public bool Connect( MainWindow window, Bluetooth bluetooth)
+        {
+            bool isConfigurationOK = Configuration();
+
+            if (isConfigurationOK == true)
+            {
+                try
+                {
+                    _serialPort.Open();
+                    IsPortOpen = ConnectionTest(window);
+                    if (IsPortOpen == true)
+                    {
+                        readThread = new Thread(() => bluetooth.Read(window));
+                        readThread.Start();
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("You have chosen the wrong port", "Port problem", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+            }
+            return IsPortOpen;
+        }
+        public bool Disconnect(MainWindow window)
+        {
+            bool isDisconnectOK = false;
+
+            IsPortOpen = false;
+            readThread.Join();
+
+            _serialPort.DiscardInBuffer();
+            _serialPort.DiscardOutBuffer();
+            _serialPort.Write("0"); // 0->DISCONNECTION COMMAND
+            string message = _serialPort.ReadTo("#");
+            if (message == "END")
+            {
+
+                isDisconnectOK = true;
+                dataReceived.Append("End of transmission\n");
+                _serialPort.Close();
+                window.Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    window.txtBox_receivedMessages.Text = dataReceived.ToString();
+                }));
+                return isDisconnectOK;
+            }
+            else
+            {
+                IsPortOpen = true;
+                return isDisconnectOK;
+            }
+        }
+        bool ConnectionTest(MainWindow window)
+        {
+            _serialPort.DiscardInBuffer();
+            _serialPort.DiscardOutBuffer();
+            _serialPort.Write("1"); // 1->CONNECTION COMMAND
+            string message = _serialPort.ReadTo("#");
+            if (message == "OK")
+            {
+                dataReceived.Append("Start of transmission\n");
+                window.Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    window.txtBox_receivedMessages.Text = dataReceived.ToString();
+                }));
+                return true;
+            }
+            else
+                return false;
+        }
+        public void SendData(string data, MainWindow window)
+        {
+            if (IsPortOpen)
+            {
+                _serialPort.Write(data);
+                dataSend.Append($"{data}\n");
+                window.Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    window.txtBox_sendMessages.Text = dataSend.ToString();
+                }));
+            }
+        }
+        public void Read(MainWindow window)
+        {
+            while (IsPortOpen)
+            {
+                try
+                {
+                    dataReceived.Append($"{_serialPort.ReadTo("#")}\n");
+                    window.Dispatcher.BeginInvoke(new Action(() =>
+                    {
+                        window.txtBox_receivedMessages.Text = dataReceived.ToString();
+                    }));
+                }
+                catch (TimeoutException) { }
+            }
+        }
     }
 }
--- CubliApp/MainWindow.xaml.cs
+++ CubliApp/MainWindow.xaml.cs
@@ -1,27 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO.Ports;
+﻿using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
+using System.IO.Ports;
+using System.Windows.Media;
+using System.Text;
+using System;
 
 namespace CubliApp
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
-        public int boudRate { get; set; }
-        public int dataBits { get; set; }
-        public string parity { get; set; }
-        public string stopBits { get; set; }
+
+        Bluetooth bluetooth = new Bluetooth();
         public MainWindow()
         {
             InitializeComponent();
+            ReScanPorts();
         }
 
-        private void btn_ReScan_Click(object sender, RoutedEventArgs e)
+        private void ReScanPorts()
         {
+            comboBox_COMS.Items.Clear();
             foreach (string s in SerialPort.GetPortNames())
             {
                 if (comboBox_COMS.Items.Contains(s) == false)
@@ -30,34 +29,72 @@ namespace CubliApp
                 }
             }
         }
+        private void btn_ReScan_Click(object sender, RoutedEventArgs e)
+        {
+            ReScanPorts();
+        }
 
         private void RadioButton_Checked(object sender, RoutedEventArgs e)
         {
             RadioButton radioButton = (RadioButton)sender;
             if (radioButton.GroupName == "BaudRate")
-                boudRate = int.Parse(radioButton.Content.ToString());
+                bluetooth.BoudRate = int.Parse(radioButton.Content.ToString());
             else if (radioButton.GroupName == "DataBits")
-                dataBits = int.Parse(radioButton.Content.ToString());
+                bluetooth.DataBits = int.Parse(radioButton.Content.ToString());
             else if (radioButton.GroupName == "Parity")
-                parity = radioButton.Content.ToString();
+                bluetooth.Parity = radioButton.Content.ToString();
             else if (radioButton.GroupName == "StopBits")
-                stopBits = radioButton.Content.ToString();
-
+                bluetooth.StopBits = radioButton.Content.ToString();
         }
 
         private void btn_Connect_Click(object sender, RoutedEventArgs e)
         {
-            SerialPort _serialPort;
+            btn_Disconnect.IsEnabled = true;
+            bool isConnection = bluetooth.Connect(this,bluetooth);
+            if (isConnection == true)
+            {
+                lbl_disconnect_connect.Foreground = Brushes.Green;
+                lbl_disconnect_connect.Content = "Connected";
+                btn_Connect.IsEnabled = false;
+            }
+        }
 
-            // Create a new SerialPort object with default settings.
-            _serialPort = new SerialPort();
-            // Allow the user to set the appropriate properties.
-            _serialPort.PortName = "COM4";
-            _serialPort.BaudRate = boudRate;
-            _serialPort.Parity = (Parity)Enum.Parse(typeof(Parity), parity, true);
-            _serialPort.DataBits = dataBits;
-            _serialPort.StopBits = (StopBits)Enum.Parse(typeof(StopBits), stopBits, true);
 
+        private void btn_Disconnect_Click(object sender, RoutedEventArgs e)
+        {
+            btn_Connect.IsEnabled = true;
+            bool isDisconnectOK = bluetooth.Disconnect(this);
+            if (isDisconnectOK == true)
+            {
+                lbl_disconnect_connect.Foreground = Brushes.Red;
+                lbl_disconnect_connect.Content = "Disconnected";
+                btn_Disconnect.IsEnabled = false;
+            }
+            else
+            {
+                MessageBox.Show("Failed to disconnect", "Port problem", MessageBoxButton.OK, MessageBoxImage.Warning);
+
+            }
+        }
+
+        private void btn_Send_Click(object sender, RoutedEventArgs e)
+        {
+            string data = txtBox_DataToSend.Text;
+            bluetooth.SendData(data,this);
+        }
+
+        private void btn_ClearAll_Click(object sender, RoutedEventArgs e)
+        {
+            bluetooth.dataReceived = new StringBuilder();
+            bluetooth.dataSend = new StringBuilder();
+            txtBox_receivedMessages.Text = bluetooth.dataReceived.ToString();
+            txtBox_sendMessages.Text = bluetooth.dataReceived.ToString();
+        }
+
+        private void comboBox_COMS_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(comboBox_COMS.SelectedValue!=null)
+                bluetooth.PortName = comboBox_COMS.SelectedValue.ToString();
         }
     }
 }
